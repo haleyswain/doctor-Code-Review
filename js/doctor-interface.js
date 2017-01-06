c838ffabf1f15aba19c317c466a136a5907a6bec
Haley  Swain <haleyswain@Haleys-MacBook-Pro.local>
2017-01-06 10:06:44 -0800

    update JS files to achieve functionality within the console

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -2,17 +2,3 @@
 
 var apiKey = require('./../.env').apiKey;
 var Doctor = require('./../js/doctor.js').doctorModule;
-
-var medicalIssue = $('#illness').val();
-
-
-$(document).ready(function() {
-  $('#showIllness').submit(function(event) {
-    $('#showDoctor').empty();
-    event.preventDefault();
-  });
-  var currentDoctorObject = new Doctor();
-  currentDoctorObject.getDoctor();
-      $('#illness').val("");
-
-});
--- js/doctor.js
+++ js/doctor.js
@@ -1,15 +1,28 @@
 // back-end file
-var physicians = [];
-
-function Doctor(){
-}
-var medicalIssue = $('#illness').val();
 var apiKey = require('./../.env').apiKey;
+function Doctor() {
+}
 
-Doctor.prototype.getDoctor = function(medicalIssue) {
-  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue +'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
-  .then(function(result) {
-      console.log(result);
+var showDoc = function(first_name, last_name) {
+  $('#showDoctor').append(first_name, last_name);
+}
+
+$(document).ready(function() {
+  var currentDoctorObject = new Doctor;
+  $("#showIllness").click(function() {
+    var medicalIssue = $("#illness").val();
+    currentDoctorObject.getDoctor(medicalIssue, showDoc);
+  })
+})
+
+Doctor.prototype.getDoctor = function(medicalIssue, showDoctor) {
+  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key='+ apiKey).then(function(result) {
+      for (i = 0; i <= 19; i++) {
+        console.log(result.data[i].profile.first_name + " " + result.data[i].profile.last_name);
+        var first_name = result.data[i].profile.first_name;
+        var last_name = result.data[i].profile.last_name;
+        $('#showDoctor').append(first_name + last_name);
+      }
     })
     .fail(function(error){
       console.log("fail");
