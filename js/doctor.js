e720c1c1f39916c19555eba1a5b43423b50920f8
Haley  Swain <haleyswain@Haleys-MacBook-Pro.local>
2017-01-06 10:20:34 -0800

    edit showDoctor append function for funcitonality in the browser

--- js/doctor.js
+++ js/doctor.js
@@ -5,15 +5,15 @@ function Doctor() {
 
 var showDoc = function(first_name, last_name) {
   $('#showDoctor').append(first_name, last_name);
-}
+};
 
 $(document).ready(function() {
-  var currentDoctorObject = new Doctor;
+  var currentDoctorObject = new Doctor();
   $("#showIllness").click(function() {
     var medicalIssue = $("#illness").val();
     currentDoctorObject.getDoctor(medicalIssue, showDoc);
-  })
-})
+  });
+});
 
 Doctor.prototype.getDoctor = function(medicalIssue, showDoctor) {
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key='+ apiKey).then(function(result) {
@@ -21,7 +21,7 @@ Doctor.prototype.getDoctor = function(medicalIssue, showDoctor) {
         console.log(result.data[i].profile.first_name + " " + result.data[i].profile.last_name);
         var first_name = result.data[i].profile.first_name;
         var last_name = result.data[i].profile.last_name;
-        $('#showDoctor').append(first_name + last_name);
+        $('.showDoctor').append(first_name + last_name);
       }
     })
     .fail(function(error){
